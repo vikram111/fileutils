6e8c6f2de7b11410730fbfd58aa4aa98ab90e2aa
vikramthakur <vikramthakur@nbnco.com.au>
2019-09-18 21:03:39 +1000

    Initial commit

---
+++ src/main/java/utils/ReadResource.java
@@ -0,0 +1,37 @@
+package utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ReadResource {
+     void loadProps(){
+        Properties properties = new Properties();
+        try(InputStream inputStream = ReadResource.class.getResourceAsStream("application.properties")){
+            if(inputStream != null){
+                properties.load(inputStream);
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Name : "+properties.getProperty("name"));
+
+    }
+
+    public static void main(String[] args) {
+        ReadResource readResource = new ReadResource();
+        readResource.loadProps();
+
+        Properties properties = new Properties();
+        try(InputStream inputStream = ReadResource.class.getResourceAsStream("application.properties")){
+            if(inputStream != null){
+                properties.load(inputStream);
+            }
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Name : "+properties.getProperty("name"));
+    }
+}
